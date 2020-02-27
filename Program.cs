90ed8506d7b5087ac522d03a85d66493816db0c8
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2020-02-28 00:45:39 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JustChess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
